04fcc374e7e073641358d02ba7ab6a2604c8d865
Michele <micheleierniero@gmail.com>
2021-09-24 12:31:40 +0200

    struttura componenti provvisoria

---
+++ src/components/Main.vue
@@ -0,0 +1,46 @@
+<template>
+  <main>
+    <!-- <ul>
+      <li v-for="(movie, index) in Movies" :key="index">
+        {{ movie.title }}
+      </li>
+    </ul> -->
+
+    <Movie v-for="(movie, index) in Movies" :key="index" :info="movie" />
+  </main>
+</template>
+
+<script>
+import axios from 'axios';
+import Movie from "./Movie.vue";
+export default {
+  name: 'Main',
+  components: {
+    Movie
+  },
+  data() {
+    return {
+      APIUrl: 'https://api.themoviedb.org/3/search/movie?api_key=4b82c92c1924d290b7b7e9d2d0132a2d&query=rick',
+      movies: [],
+    }
+  },
+  created() {
+    this.getMovies()
+  },
+  methods: {
+    getMovies() {
+      axios
+        .get(this.APIUrl)
+        .then( result => {
+          // console.log(result);
+          this.movies = result.data.results
+        })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/Movie.vue
@@ -0,0 +1,23 @@
+<template>
+  <main>
+    <ul>
+      <img :src="info.poster_path" :alt="info.title">
+      <li>
+        {{ info.title }}
+      </li>
+    </ul>
+  </main>
+</template>
+
+<script>
+
+export default {
+  name: 'Movie',
+  props: ['info']
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
